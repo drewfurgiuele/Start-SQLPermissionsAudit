ef574845262d54fbb162508dc0597856e4ca37e9
Drew Furgiuele <drewfurgiuele@gmail.com>
2015-08-31 20:34:00 -0400

    Updated help information

--- Start-SQLPermissionsAudit/Start-SQLPermissionsAudit.ps1
+++ Start-SQLPermissionsAudit/Start-SQLPermissionsAudit.ps1
@@ -1,4 +1,41 @@
 ﻿<#
+.SYNOPSIS
+    Takes a given SQL server (or database on a SQL server) and audits people who have write access. If there are AD groups, it finds all users in those groups (and groups within groups).
+.DESCRIPTION
+    This script will attempt to scan all domain users and groups in the Windows Active Directory domain and report out on who has write access to your database(s). It will also recursively scan AD groups if a group contains other groups.
+	NOTE: This script requires the SQLPS module. You can get it by installing SQL Server Management Tools, then by importing the module:
+
+	Import-Module SQLPS
+
+	ALSO NOTE: This script also requires the Windows Remote Management tools installed on your machine, or, if you're running from a server, the AD Domain Services Windows feature. You can get either by:
+
+		A) Installing Windows Remote Server Administration Tools, or
+		B) Running the "Install-windowsfeature -name AD-Domain-Services –IncludeManagementTools" command in PowerShell
+
+	This will install the AD module that this script makes use of.
+
+	You can also customize the script to look for other server/database roles or permissions by modifying the $LookingForGroups or $LookingForObjectPermissions arrays.
+
+.PARAMETER servername
+    The hostname or FQDN of the server you want the script to scan.
+.PARAMETER instancename
+    The instance name of the SQL server you want the script to run on, defaults to "DEFAULT" for non-named instances. This is optional parameter.
+.PARAMETER databasename
+    The name of the database you want to collect write permissions on. If not provided, the script will scan all databases. This is an optional parameter.
+.EXAMPLE
+    ./Start-SQLPermissionsAudit -Servername localhost
+
+	This will execute a permissions audit on the server 'localhost' in ALL databases
+.EXAMPLE
+    ./Start-SQLPermissionsAudit -Servername localhost -databasename AdventureWorks2014
+
+	This will execute a permissions audit on the server 'localhost' in ONLY the AdventureWorks 2014 database
+.OUTPUTS
+    The script will output a table full of permissions.
+.NOTES
+    .
+.CHANGELOG
+    .
 #>
 param(
     [Parameter(Mandatory=$true)] [string] $servername,
